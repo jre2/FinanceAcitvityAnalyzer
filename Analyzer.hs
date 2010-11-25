64d032729c6854b46b80fc402a0f08119d5f8c44
Joseph Re <overture2112@gmail.com>
2010-11-24 21:53:40 -0500

    improved

--- Analyzer.hs
+++ Analyzer.hs
@@ -2,19 +2,12 @@
 
 import qualified Data.Chase.Activity as Chase
 import Finance.Activity
+import Data.Tag
 
 import qualified Data.ByteString.Char8 as B
-import Data.Either
-import Data.List
-import Data.Maybe
-import Data.Tag
-import Data.Time
-import Data.Ord
-import qualified Text.CSV as CSV
 import Text.Printf
 import Text.Regex.PCRE.Light
 import System.IO.Unsafe
-import System.Locale
 
 ruleStrs =
     -- food
@@ -85,21 +78,21 @@ ruleStrs =
 
 -- Main
 rules = mkRules [caseless] ruleStrs
+
 trans' = unsafePerformIO $ Chase.loadTrans "JPMC.CSV"
 trans = mkTaggedWrt [] rules trans' msg
 main = do
    ppm $ withTag "food" trans
    print $ amtOutMonthly 2010 trans
 
--- Debugging
-needRule = (pp $ map msg $ untagged trans) >> (print $ length $ untagged trans)
-haveTag t = pp $ withTag t trans
-msgs = map (msg . fst) trans
-fooMsg = msg $ fst $ head $ trans
-
 -- Pretty Printing
 pp :: (Show a) => [a] -> IO ()
 pp = mapM_ (putStrLn . show)
 
-ppm = pp . (map (\x -> printf "%8.2f %s" (amt x) (f $ B.unpack $ msg x) ) :: [Tran]->[String]) . map fst
-    where f m = m
+ppm = pp . (map (\x -> printf "%8.2f %s" (amt x) (B.unpack $ msg x) ) :: [Tran]->[String]) . map fst
+
+-- Debugging while writting rules
+needRule = (pp $ map msg $ untagged trans) >> (print $ length $ untagged trans)
+haveTag t = pp $ withTag t trans
+msgs = map (msg . fst) trans
+fooMsg = msg $ fst $ head $ trans
